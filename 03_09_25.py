2025407af92e968d1e579d16c97c5f9b2d07ce89
John Doe <your_email@abc.example>
2025-09-03 12:48:36 -0500

    first commit

---
+++ 03_09_25.py
@@ -0,0 +1,17 @@
+#Autor: José Daniel Salcedo Gutiérrez
+#Condicionales 2: booleanos
+
+
+x = int(input("Ingrese un nnumero entero del 0 al 5: \n"))
+
+a = 5 == x
+
+if ((a != False) and (True)):
+    print("La premisa es verdadera")
+else: 
+    print("La premisa es falsa")
+
+print(type(a),a)    
+
+
+#else if hacer 10 ejercicios.
